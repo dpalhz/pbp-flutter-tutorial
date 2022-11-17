5a0faceddd179f7c3c0b8cdfef1779337f48625d
dipa.alhaza <dipa.alhaza@ui.ac.id>
2022-11-17 13:30:59 +0700

    tutorial labb 8

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:first_flutter/form.dart';
+import 'package:first_flutter/page/form.dart';
+import 'package:first_flutter/page/to_do_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -66,6 +67,16 @@ class _MyHomePageState extends State<MyHomePage> {
                 Navigator.pushReplacement(
                   context,
                   MaterialPageRoute(builder: (context) => const MyFormPage()),
+                );
+              },
+            ),
+            ListTile(
+              title: const Text('To Do'),
+              onTap: () {
+                // Route menu ke halaman to do
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const ToDoPage()),
                 );
               },
             ),
